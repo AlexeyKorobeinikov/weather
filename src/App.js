8aef29f7ec435e548128cc76909d88e6d894bc07
AlexeyKorobeinikov <lekskor@mail.ru>
2022-06-06 12:49:45 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React, { useState } from 'react';  
-  
+import React, { useState } from 'react';
+
 export default function App() {  
-  const [name, setName] = useState('');  
+  const [name, setName] = useState('');
   const Cityes = ['Анапа', 'Белгород', 'Волгоград', 'Вологда', 'Владивосток', 'Екатеринбург', 'Ижевск', 'Краснодар', 'Красноярск', 'Липецк', 'Москва', 'Мурманск', 'Новосибирск', 'Норильск', 'Омск', 'Пенза', 'Ростов', 'Рязань', 'Санкт-Петербург', 'Саратов', 'Смоленск', 'Тамбов', 'Томск', 'Тюмень', 'Ульяновск', 'Уфа', 'Хабаровск',];
-  //for (let i=0, i < Cityes.length, i++) {
-    //if {name}==Cityes[i];
+
+    function handleChange(e)
+    {
+        for (let i=0; i < Cityes.length; ++i)
+        {
+            if (e.target.value == Cityes[i])
+            {
+                setName(e.target.value);
+            }
+        }
+    }
+
   return (  
     <div>  
       <form>
         <br />  
         <label>Введите название города:</label>  
-        <input type="text" onChange={(e) => setName(e.target.value)} />  
+        <input type="text" onChange={handleChange} />
       </form>  
       <br />  
            Город: {name}  
